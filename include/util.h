12b57a84eebde5013c730ef3edb6369339e9b1dd
NathanNeurotic (Ripto) <109461996+NathanNeurotic@users.noreply.github.com>
2025-12-29 05:04:02 -0800

    Harden getMountInfo buffer handling

--- include/util.h
+++ include/util.h
@@ -1,3 +1,5 @@
+#include <stddef.h>
+
 /**
  * @brief checks for file existence
  * @param filepath path to the file to check
@@ -26,14 +28,17 @@ int get_CNF_string(char **CNF_p_p,
                    char **value_p_p);
 
 /**
- * @brief  method returns true if it can extract needed info from path, otherwise false.
- * In case of true, it also updates mountString, mountPoint and newCWD parameters
+ * @brief  method returns 0 if it can extract needed info from path, otherwise a negative error code.
+ * In case of success, it also updates mountString, mountPoint and newCWD parameters
  * It splits path by ":", and requires a minimum of 3 elements
  * @example if path = hdd0:__common:pfs:/retroarch/ then: mountString = "pfs:", mountPoint = "hdd0:__common", newCWD = pfs:/retroarch/
  * @param path input parameter with full hdd path (`hdd0:__common:pfs:/retroarch/`)
  * @param mountString pointer to char* wich will contain pfs mountpoint (`pfs:`)
+ * @param mountStringSize total size of the mountString buffer
  * @param mountPoint returns the path of mounted partition (`hdd0:__common`)
+ * @param mountPointSize total size of the mountPoint buffer
  * @param newCWD returns the path to the file as pfs mount point string
- * @return true on success
+ * @param newCWDSize total size of the newCWD buffer
+ * @return 0 on success, negative error code on failure or truncation
 */
-int getMountInfo(char *path, char *mountString, char *mountPoint, char *newCWD);
+int getMountInfo(char *path, char *mountString, size_t mountStringSize, char *mountPoint, size_t mountPointSize, char *newCWD, size_t newCWDSize);
