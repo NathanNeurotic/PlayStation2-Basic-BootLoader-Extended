b6f04bebad14500e3c2112b144dcd77172a78110
NathanNeurotic (Ripto) <109461996+NathanNeurotic@users.noreply.github.com>
2026-01-07 15:28:07 -0800

    Cppcheck stubs: minimize string header

--- include/cppcheck_stubs/stddef.h
+++ include/cppcheck_stubs/stddef.h
@@ -1,11 +1,7 @@
 #pragma once
 // Stub for Codacy/Cppcheck static analysis; not used in PS2SDK builds.
 
-#ifndef __SIZE_TYPE__
-typedef unsigned long size_t;
-#else
-typedef __SIZE_TYPE__ size_t;
-#endif
+typedef unsigned int size_t;
 
 #ifndef NULL
 #define NULL ((void *)0)
--- include/cppcheck_stubs/string.h
+++ include/cppcheck_stubs/string.h
@@ -1,14 +1,4 @@
 #pragma once
 // Stub for Codacy/Cppcheck static analysis; not used in PS2SDK builds.
 
-typedef unsigned long size_t;
-
-void *memcpy(void *dest, const void *src, size_t n);
-void *memset(void *s, int c, size_t n);
-int memcmp(const void *s1, const void *s2, size_t n);
-size_t strlen(const char *s);
-int strcmp(const char *s1, const char *s2);
-int strncmp(const char *s1, const char *s2, size_t n);
-char *strstr(const char *haystack, const char *needle);
-char *strtok(char *str, const char *delim);
-char *strdup(const char *s);
+#include "stddef.h"
